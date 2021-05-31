5cd97bbc32664079d66e5feb27dcc1f0d46b4a4d
AlekSerikov <53313986+AlekSerikov@users.noreply.github.com>
2021-05-31 14:39:34 +0300

    Add files via upload

---
+++ src/main/java/com/example/jenkinsWebhooks/JenkinsWebhooksApplication.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsWebhooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsWebhooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsWebhooksApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getMessage (){
+		return "I'm from webhook!";
+	}
+}
